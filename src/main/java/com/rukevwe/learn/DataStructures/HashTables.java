5d693611a1e9645d9bd0973193a38f4a59921662
Ahwin Oghenerukevwe <oahwin@seamfix.com>
2019-11-24 12:47:14 +0100

    Implemented several data structures

--- src/main/java/com/rukevwe/learn/DataStructures/HashTables.java
+++ src/main/java/com/rukevwe/learn/DataStructures/HashTables.java
@@ -2,7 +2,15 @@ package com.rukevwe.learn.DataStructures;
 
 public class HashTables {
     
-    // For any problem, have hash tables at the top of your mind
+    /*
+     For any problem, have hash tables at the top of your mind,
+     Implement HashTable.
+     
+     Collision Handling
+     Understand how HastTables grow/shrink.
+     
+     Key -> hashcode -> index.
+     */
     
     
 }
---
+++ src/main/java/com/rukevwe/learn/InterviewProblems/PhoneBook.java
@@ -0,0 +1,4 @@
+package com.rukevwe.learn.InterviewProblems;
+
+public class PhoneBook {
+}
